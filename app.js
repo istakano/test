b11367cd6e94da92f537033dee3d3ead4a2653b2
yoshihiro.takano <yoshihiro.takano@i-studio.co.jp>
2017-01-30 20:47:16 +0900

    git pull master

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("test");
+/*
+ * 同期的に処理を実行したい場合にはexecSync
+ *
+ * 参考：http://tkybpp.hatenablog.com/entry/2016/04/25/163246
+ */
+const execSync = require('child_process').execSync;
+const result =  execSync('git pull master');
+console.log(result);
